1ed45d926c5e2064f0266b5ed2253cfe487e5de7
Minwoo Lee <ermaker@gmail.com>
2019-06-07 21:54:13 +0900

    Add auth for es and kibana

--- master/setup_es.sh
+++ master/setup_es.sh
@@ -4,10 +4,11 @@ curl --noproxy localhost -XPUT 'http://localhost:9200/_template/index_setting?pr
   "index_patterns": ["*"],
   "order": 0,
   "settings": {
-    "number_of_shards" : "1",
     "number_of_replicas" : "0"
   }
 }'
+
+exit
 
 curl --noproxy localhost -XPUT 'http://localhost:9200/_template/fluentd?pretty=true' -H 'Content-Type: application/json' -d '{
   "index_patterns": ["fluentd-*"],
