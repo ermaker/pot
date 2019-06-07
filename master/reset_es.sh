c5b73e0043af5926cb5abd76be4969f4fe6c5c28
Minwoo Lee <ermaker@gmail.com>
2019-06-07 22:23:14 +0900

    Fix

--- master/reset_es.sh
+++ master/reset_es.sh
@@ -2,4 +2,4 @@ set -xe
 
 export $(grep -v '^#' .env | xargs)
 
-curl --noproxy localhost -XDELETE 'http://localhost:9200/*?pretty=true'
+curl --noproxy localhost -XDELETE "http://${ES_USERNAME}:${ES_PASSWORD}@localhost:9200/*?pretty=true"
